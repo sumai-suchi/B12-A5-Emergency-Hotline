878c01ea0279827fed3420b753d340819301e218
sumaiya <sumaiyamoina@gmail.com>
2025-08-28 23:57:28 +0600

    adding the on click copy function and some error corection

--- emergency.js
+++ emergency.js
@@ -12,44 +12,40 @@ function addHeartCount() {
 function ReduceStar(target) {
     let starCount = document.getElementById('star')
     let starCountInt = parseInt(starCount.innerText)
-    let Service = target.parentNode.parentNode.childNodes[3];
+    let Service = target.closest(".card");
     console.log(Service)
+   let title=Service.querySelector("h1").innerText;
+   let serviceName=Service.querySelector("h2").innerText;
+   let number=Service.querySelector("p").innerText;
+   console.log(title,serviceName,number)
+    // console.log(Service.children)
 
     if (starCountInt >= 20) {
         starCount.innerText = starCountInt - 20;
 
-        let alertMassage = Service.childNodes;
-        console.log(alertMassage)
+    
 
 
-        alert(`${alertMassage[1].innerText}
+       alert(`${serviceName}
              
-            Calling ${alertMassage[3].innerText} service ${alertMassage[5].innerText}... `
+            Calling ${title}  ${number}... `
 
         )
            let now = new Date();
              let currentTime = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
         let history = document.createElement('div');
         history.innerHTML = `
-        <div class=" flex justify-between items-center gap-1 my-2">
-           <div><h4 class="font-bold">${alertMassage[1].innerText} </h4>
-         <p class="text-gray-400">${alertMassage[5].innerText}</p></div>
-           <div><p>${currentTime}</p></div>
+        <div class="w-full flex justify-between items-center gap-1 my-2">
+           <div class="w-4/6"><h4 class="font-bold">${title} </h4>
+         <p class="text-gray-400">${number}</p></div>
+           <div class="w-2/6"><p>${currentTime}</p></div>
         </div>`
 
        
       
 
         
-
-
-
-
-
-
-
-
-        let historyContainer = document.getElementById('History-container');
+let historyContainer = document.getElementById('History-container');
 
         historyContainer.appendChild(history)
     }
@@ -76,6 +72,9 @@ console.log(callHis)
 for (let call of callHis) {
 
     call.addEventListener('click', function (e) {
+
+        e.preventDefault();
+     
         ReduceStar(e.target)
         console.log(e.target)
     })
@@ -94,18 +93,50 @@ document.getElementById('clear-History').addEventListener('click', function () {
 //copy section
 
 
-let copies = document.getElementsByClassName('copy-text');
+let copies = document.getElementsByClassName('copy-btn');
 
 for (let copy of copies) {
 
-    copy.addEventListener('click', function () {
+    copy.addEventListener('click', async()=> {
         alert('Information has been copied')
         let copyCount = document.getElementById('copyCount');
         let copyCountInt = parseInt(copyCount.innerText);
         copyCount.innerText = copyCountInt + 1;
+    
+       let card=copy.closest('.card');
+       console.log(card)
+       const textItem=card.querySelector(".copy-num") 
+       console.log(textItem)
+       const text=textItem.innerText;
+
+       try {
+
+        await navigator.clipboard.writeText(text)
+     
+        
+       } catch (error) {
+        
+           const t=document.createElement('textarea');
+        t.value=text;
+        document.body.appendChild(t);
+        t.select();
+        document.execCommand('copy');
+        document.body.removeChild(t)
+
+       }
+
+
+
     })
 
 }
 
+//Copy paste section
 
+// let copyItem=document.getElementsByClassName('copy-btn')
+// console.log(copyItem,'hello')
 
+// for (const copy of copyItem) {
+
+    
+// }
